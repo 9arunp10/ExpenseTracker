75d07a326a1fb38a467979486bc2188a1eec3438
Arun Pachori <arunpachori10@gmail.com>
2025-06-25 20:58:45 +0530

    day4 add expense section

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Day 2: Set Budget and Show It
-
+// Set Budget and Show It
 let budget = 0;
-
 function setBudget() {
   const budgetInput = document.getElementById("budget");
   const budgetValue = parseFloat(budgetInput.value);
@@ -10,13 +8,10 @@ function setBudget() {
     alert("Please enter a valid budget greater than 0.");
     return;
   }
-
   budget = budgetValue;
-
   // Show it on the page
   const display = document.getElementById("budget-display");
   display.textContent = `Budget: ₹${budget}`;
-
   // Clear the input field after setting
   budgetInput.value = "";
 }
